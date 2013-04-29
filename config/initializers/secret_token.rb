5cc7f572552d0aada43278f75ba511d5232604b5
Gabriel Mansour <gabrielmansour@gmail.com>
2013-04-29 15:56:33 -0400

    Initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orthodemocracy::Application.config.secret_token = 'c69a1d68fbc97a7699c895d452ba98e7b3eb271a89d27b3d3e19a66e9e51146fe24462b7fe9b174161cd453d910046f8ba8dedc848aa534c0156f75dd9b26db2'
