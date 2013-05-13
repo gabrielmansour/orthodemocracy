ba6641324558b544305dcbee032f213a94c262c9
Gabriel Mansour <gabrielmansour@gmail.com>
2013-05-13 03:04:26 -0400

    Use Rails 4

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
 Orthodemocracy::Application.config.session_store :cookie_store, key: '_orthodemocracy_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Orthodemocracy::Application.config.session_store :active_record_store
